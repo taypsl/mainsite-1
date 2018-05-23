426c7435c20d80d048efdc3e767259a45b6e1ecf
Taylor Parsell <tayparsell@gmail.com>
2018-05-22 17:00:51 -0700

    mobile responsive icons

--- client/src/components/template/topbar.js
+++ client/src/components/template/topbar.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Search from './search';
 import Languages from './languages';
+import Bot from '../chatbot/Bot.jsx'; 
 
 export default class Topbar extends Component {
 	render() {
 		return (
 			<div className="Topbar" >
 				<Search />
-				<div className="Search-m"><i className="material-icons">find_in_page</i></div>
+				<div className="Search-m"><Link to="/search"><i className="material-icons">find_in_page</i></Link></div>
 				<Languages />
 				<div className="Languages-m"><i className="material-icons">language</i></div>
+				<div className="Bot-div-m"><Link to="/mbot"><i className="material-icons">chat</i></Link></div>
 			</div>
 		)
 	}
